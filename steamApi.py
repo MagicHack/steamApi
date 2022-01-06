8484a524652a9ff8941e557a3e220886ad5cd145
Émile Larose-Levac <emile.larose@gmail.com>
2022-01-06 12:55:27 -0500

    reformat pep8

--- steamApi.py
+++ steamApi.py
@@ -1,32 +1,34 @@
 from flask import Flask
 from flask import request
 import json
-from steamTest import findAppId, findName, getLastFileUpdate
+from steamTest import find_app_id, find_name, get_last_file_update
 import urllib.request
 import time
 
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "<h1>Hello There! Work in progress :)</h1>"
 
+
 @app.route('/steam/lastupdate')
 def lastUpdate():
-    timeSeconds = getLastFileUpdate()
+    timeSeconds = get_last_file_update()
 
-    format = request.args.get('format', default = 's', type = str)
+    format = request.args.get('format', default='s', type=str)
 
     if format != "s":
         return time.strftime(format.replace('$', '%'), time.gmtime(timeSeconds))
     else:
-       return str(timeSeconds)
+        return str(timeSeconds)
+
 
 @app.route('/steam/players/pajbot/<path:gameName>')
 def playersText(gameName):
-    numberSeparator = ',' # '\xa0' twitch seems to remove non breaking spaces Sadge
-    game = json.loads(findAppId(gameName))
+    numberSeparator = ','  # '\xa0' twitch seems to remove non breaking spaces Sadge
+    game = json.loads(find_app_id(gameName))
     steamApiUrl = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid="
     error = False
     res = ""
@@ -42,11 +44,19 @@ def playersText(gameName):
         playerString = "player"
         if playersOnline != 1:
             playerString += "s"
-        res = "The game {0} currently has {1} {2} online. https://steamdb.info/app/{3}/graphs".format(game["name"], f"{playersOnline:,}".replace(',', numberSeparator), playerString, game["appid"])
+        res = "The game {0} currently has {1} {2} online. https://steamdb.info/app/{3}/graphs".format(game["name"],
+                                                                                                      f"{playersOnline:,}".replace(
+                                                                                                          ',',
+                                                                                                          numberSeparator),
+                                                                                                      playerString,
+                                                                                                      game["appid"])
     else:
-        res = "Found game {0} https://steamdb.info/app/{1} but did not get a player count from steam Sadge".format(game["name"], game["appid"])
+        res = "Found game {0} https://steamdb.info/app/{1} but did not get a player count from steam Sadge".format(
+            game["name"], game["appid"])
     res += " (Match score {0}%)".format(game["matchscore"])
     return res
+
+
 @app.route('/steam/players/<int:appid>')
 def playersOnline(appid):
     steamApiUrl = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid="
@@ -54,32 +64,36 @@ def playersOnline(appid):
         response = urllib.request.urlopen(steamApiUrl + str(appid))
     except Exception as ex:
         return str(ex)
-    paramText = request.args.get('text', default = 'false', type = str)
+    paramText = request.args.get('text', default='false', type=str)
     if response.getcode() != 200:
         return "Error getting online player count, code : {}".format(response.getcode())
     else:
         playersOnline = json.loads(response.read())["response"]["player_count"]
         if paramText == "true":
-            return "The game {} currently has {} players online.".format(findName(appid), playersOnline)
+            return "The game {} currently has {} players online.".format(find_name(appid), playersOnline)
         else:
             return str(playersOnline)
 
+
 @app.route('/steam/id/<string:gameName>')
 def steamIdOnly(gameName):
-    return str(json.loads(findAppId(gameName))["appid"])
+    return str(json.loads(find_app_id(gameName))["appid"])
+
 
 @app.route('/steam/<string:gameName>')
 def steam(gameName):
     if gameName == "":
         return "0"
     else:
-        jsonResponse = findAppId(gameName)
-        paramText = request.args.get('text', default = 'false', type = str)
+        jsonResponse = find_app_id(gameName)
+        paramText = request.args.get('text', default='false', type=str)
         if paramText == "true":
             decoded = json.loads(jsonResponse)
-            return "Found the game {0} with the appid {1} (match confidence {2}%), https://store.steampowered.com/app/{1}/".format(decoded["name"], decoded["appid"], decoded["matchscore"])
+            return "Found the game {0} with the appid {1} (match confidence {2}%), https://store.steampowered.com/app/{1}/".format(
+                decoded["name"], decoded["appid"], decoded["matchscore"])
         else:
             return jsonResponse
+
 
 @app.route('/repeat/<string:text>/<int:number>')
 def repeat(text, number):
@@ -92,6 +106,7 @@ def repeat(text, number):
         repeats = number
 
     return text * repeats
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- steamTest.py
+++ steamTest.py
@@ -11,68 +11,76 @@ data = None
 
 steamGameListUrl = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/"
 
-def getLastFileUpdate():
-  file = pathlib.Path(jsonGameFile)
-  if file.exists():
-    return round(time.time() - file.stat().st_mtime)
-  return -1
 
-def readGameFile():
-  global data
-  with open(jsonGameFile) as f:
-    data = json.load(f)
-    f.close()
+def get_last_file_update():
+    file = pathlib.Path(jsonGameFile)
+    if file.exists():
+        return round(time.time() - file.stat().st_mtime)
+    return -1
 
-def updateSteamFile():
-  opener = urllib.request.build_opener()
-  opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.67')]
-  urllib.request.install_opener(opener)
-  urllib.request.urlretrieve (steamGameListUrl, "steam.json")
 
-def checkUpdateGameData():
-  updateFrequency = 12 * 60 * 60 # How often to refresh the json in seconds
-  file = pathlib.Path(jsonGameFile)
-  if file.exists():
-    timeSinceLastUpdate = time.time() - file.stat().st_mtime
-    if timeSinceLastUpdate > updateFrequency:
-      print("Updating file, last update {} hours ago".format(timeSinceLastUpdate/60/60))
-      updateSteamFile()
-      readGameFile()
-  else:
-    print("json not found, fetching it")
-    updateSteamFile()
-    readGameFile()
+def read_game_file():
+    global data
+    with open(jsonGameFile) as f:
+        data = json.load(f)
+        f.close()
 
-def findName(appid):
-  for app in data["applist"]["apps"]:
-    if app["appid"] == appid:
-      return app["name"]
-  return "Error : game not found"
 
-def findAppId(name):
-  checkUpdateGameData()
-  readGameFile()
-  bestMatchName = ""
-  bestMatch = -1
-  bestRatio = 0
-  for app in data["applist"]["apps"]:
-    appName = app["name"]
-    searchName = name
-    appId = app["appid"]
-    fuzzRatio = fuzz.ratio(appName.lower(), searchName.lower())
-    if fuzzRatio > bestRatio:
-      bestRatio = fuzzRatio
-      bestMatch = appId
-      bestMatchName = appName
-      if bestRatio == 100:
-        break
-  print("Found app {} with fuzz ratio of {}, id {}".format(bestMatchName, bestRatio, bestMatch))
-  response = {"appid" : bestMatch, "name" : bestMatchName, "matchscore" : bestRatio}
-  return json.dumps(response)
+def update_steam_file():
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent',
+                          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.67')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(steamGameListUrl, "steam.json")
+
+
+def check_update_game_data():
+    update_frequency = 12 * 60 * 60  # How often to refresh the json in seconds
+    file = pathlib.Path(jsonGameFile)
+    if file.exists():
+        time_since_last_update = time.time() - file.stat().st_mtime
+        if time_since_last_update > update_frequency:
+            print("Updating file, last update {} hours ago".format(time_since_last_update / 60 / 60))
+            update_steam_file()
+            read_game_file()
+    else:
+        print("json not found, fetching it")
+        update_steam_file()
+        read_game_file()
+
+
+def find_name(appid):
+    for app in data["applist"]["apps"]:
+        if app["appid"] == appid:
+            return app["name"]
+    return "Error : game not found"
+
+
+def find_app_id(name):
+    check_update_game_data()
+    read_game_file()
+    best_match_name = ""
+    best_match = -1
+    best_ratio = 0
+    for app in data["applist"]["apps"]:
+        app_name = app["name"]
+        search_name = name
+        app_id = app["appid"]
+        fuzz_ratio = fuzz.ratio(app_name.lower(), search_name.lower())
+        if fuzz_ratio > best_ratio:
+            best_ratio = fuzz_ratio
+            best_match = app_id
+            best_match_name = app_name
+            if best_ratio == 100:
+                break
+    print("Found app {} with fuzz ratio of {}, id {}".format(best_match_name, best_ratio, best_match))
+    response = {"appid": best_match, "name": best_match_name, "matchscore": best_ratio}
+    return json.dumps(response)
 
 
 # while True:
 #   name = input("App name to search for: ")
 #   findAppId(name)
 
-checkUpdateGameData()
+check_update_game_data()
